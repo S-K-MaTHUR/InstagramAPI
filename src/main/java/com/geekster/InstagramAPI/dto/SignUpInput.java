80aba04e1edc3972612b70e98764d54d9df23efd
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-15 03:33:03 +0530

    Update SignUpInput.java

--- src/main/java/com/geekster/InstagramAPI/dto/SignUpInput.java
+++ src/main/java/com/geekster/InstagramAPI/dto/SignUpInput.java
@@ -1,4 +1,28 @@
 package com.geekster.InstagramAPI.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpInput {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private Integer age;
+
+    private String email;
+
+    @Size(min = 6,max = 18,message = "Length of the must must lie inbetween 6 to 18 characters")
+    private String userPassword;
+
+    private String phoneNumber;
 }
