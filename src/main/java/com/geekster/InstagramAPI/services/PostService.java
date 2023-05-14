06af2000896eadd41ee9fdda1a4d253be688205a
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-15 03:37:15 +0530

    Update PostService.java

--- src/main/java/com/geekster/InstagramAPI/services/PostService.java
+++ src/main/java/com/geekster/InstagramAPI/services/PostService.java
@@ -1,4 +1,22 @@
 package com.geekster.InstagramAPI.services;
 
+import com.geekster.InstagramAPI.model.Post;
+import com.geekster.InstagramAPI.repositories.IPostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+
+    @Autowired
+    IPostRepo postRepo;
+
+    public List<Post> getAllPost(){
+        return postRepo.findAll();
+    }
+    public Post savePost(Post post){
+        return postRepo.save(post);
+    }
 }
