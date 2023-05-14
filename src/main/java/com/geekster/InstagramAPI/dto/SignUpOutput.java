31c2e2089cecc0d029bdcb7868007b863c9c144b
Shubham Kumar Mathur <113419643+S-K-MaTHUR@users.noreply.github.com>
2023-05-15 03:33:27 +0530

    Update SignUpOutput.java

--- src/main/java/com/geekster/InstagramAPI/dto/SignUpOutput.java
+++ src/main/java/com/geekster/InstagramAPI/dto/SignUpOutput.java
@@ -1,4 +1,15 @@
 package com.geekster.InstagramAPI.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpOutput {
+
+    HttpStatus status;
+    String message;
 }
